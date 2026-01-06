596d2cc0d660116ed24e256567d90c46bfbf5475
amitpaul2004 <pal900290@gmail.com>
2026-01-06 11:15:44 +0530

    first commit

---
+++ enter.py
@@ -0,0 +1,13 @@
+import pyautogui
+import time
+
+# Time to switch to WhatsApp chat (5 seconds)
+time.sleep(5)
+
+# Number of times to send "Sorry"
+repeat = 100
+
+for i in range(repeat):
+    pyautogui.typewrite("Hii ")
+    pyautogui.press("enter")
+    time.sleep(2)
